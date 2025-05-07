94bd7bdbe74821b8d10b8a9c1b17c27518cee488
Tony <guerra.anthony122@gmail.com>
2025-05-07 02:29:17 -0300

    [FIX]
    Fixing filenames

--- src/main/java/com/tonyguerra/ytplayer/controllers/VideoCardController.java
+++ src/main/java/com/tonyguerra/ytplayer/controllers/VideoCardController.java
@@ -1,5 +1,9 @@
 package com.tonyguerra.ytplayer.controllers;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import com.tonyguerra.ytdownloader.dto.Video;
 import com.tonyguerra.ytdownloader.utils.YtUtils;
 import com.tonyguerra.ytplayer.components.Toast;
@@ -51,10 +55,57 @@ public final class VideoCardController {
     private void onDownloadAction(ActionEvent event) {
         Platform.runLater(() -> Toast.showToast(videoCard, "Downloading " + video.title()));
         new Thread(() -> {
-            YtUtils.downloadVideo(video, "video");
+            final String videoFolder = Path.of(System.getProperty("user.home"), "Videos", "YTPlayer").toString()
+                    + File.separator;
+
+            new File(videoFolder).mkdirs();
+
+            YtUtils.downloadVideo(video, videoFolder);
             Platform.runLater(() -> {
                 Toast.showToast(videoCard, "Download complete");
             });
+
+            final String os = System.getProperty("os.name").toLowerCase();
+
+            try {
+                final String title = video.title().replaceAll("[^a-zA-Z0-9\\s]", "_");
+
+                final String filePath = testAndGetExtFile(videoFolder + title);
+
+                if (os.contains("win")) {
+                    new ProcessBuilder("cmd", "/c", "start", "", filePath).start();
+                } else if (os.contains("nix")) {
+                    new ProcessBuilder("xdg-open", filePath).start();
+                } else {
+                    System.err.println("Unsupported OS: " + os);
+                }
+            } catch (Exception ex) {
+                Platform.runLater(() -> {
+                    Toast.showToast(videoCard, "Error opening video file");
+                });
+
+                ex.printStackTrace();
+            }
         }).start();
+    }
+
+    private static String testAndGetExtFile(String file) {
+        if (Files.exists(Path.of(file + ".webm"))) {
+            return file + ".webm";
+        } else if (Files.exists(Path.of(file + ".mp4"))) {
+            return file + ".mp4";
+        } else if (Files.exists(Path.of(file + ".mkv"))) {
+            return file + ".mkv";
+        } else if (Files.exists(Path.of(file + ".flv"))) {
+            return file + ".flv";
+        } else if (Files.exists(Path.of(file + ".avi"))) {
+            return file + ".avi";
+        } else if (Files.exists(Path.of(file + ".mov"))) {
+            return file + ".mov";
+        } else if (Files.exists(Path.of(file + ".wmv"))) {
+            return file + ".wmv";
+        } else {
+            return null;
+        }
     }
 }
